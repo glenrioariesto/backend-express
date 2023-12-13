861f2d3109204d885f5008d94894162d32f5b807
glenrioariesto <glenrioariesto@gmail.com>
2023-12-13 19:35:30 +0700

    config json

--- index.js
+++ index.js
@@ -12,7 +12,16 @@ app.use(UserRoute);
 app.use(partnersRouter);
 app.use(doctorRouter);
 app.use(appointmentsRouter);
+const corsOptions = {
+  origin: "https://backend-express-production-3d26.up.railway.app", // replace with your frontend's domain
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true, // enable credentials (cookies, authorization headers, etc.)
+};
 
+app.get("/", cors(corsOptions), (req, res) => {
+  // Your specific route handler logic goes here
+  res.send("Hello from specific route!");
+});
 const port = 3001;
 
 app.listen(port, () =>
